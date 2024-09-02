f2d77164f9cca613dde581d0f293534bde6e96c2
Tharindu Saram <113196213+TharinduSaram@users.noreply.github.com>
2024-09-02 14:07:11 +0200

    Add files via upload
    Upload Minimal API Sample Project with DTO and TypedResults

---
+++ MinimalAPITypedResultsAndDTO/MinimalAPITypedResultsAndDTO/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPITypedResultsAndDTO
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? CreatedBy { get; set; }
+    }
+}
